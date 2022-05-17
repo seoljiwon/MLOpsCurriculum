18c1fd9cee6877c900a39e55b3e04c55f1e3bc6c
seoljiwon <suljiwon@naver.com>
2022-05-17 16:45:03 +0900

    fix: use user interface, fix exception

--- api/project/src/users/users.service.ts
+++ api/project/src/users/users.service.ts
@@ -4,7 +4,7 @@ import {
   ConflictException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from './dto/users.dto';
+import { UserParams } from './inferface/user.interface';
 import { User } from './entity/user.entity';
 import { UserRepository } from './entity/user.repository';
 
@@ -15,8 +15,6 @@ export class UsersService {
     private userRepository: UserRepository,
   ) {}
 
-  private readonly users: User[] = [];
-
   healthCheck() {
     const result = {
       status: 'OK',
@@ -24,59 +22,63 @@ export class UsersService {
     return result;
   }
 
-  async create(user: CreateUserDto): Promise<User> {
-    if (user.name) {
-      const existingName = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.name = :name', { name: user.name })
-        .getOne();
-      if (existingName) {
+  async create(user: UserParams) {
+    let result;
+    try {
+      result = await this.userRepository.save(user);
+    } catch (err) {
+      // conflict error
+      if (err.code === 'ER_DUP_ENTRY')
         throw new ConflictException('The user already exists.');
-      }
     }
-    const result = await this.userRepository.save(user);
+
     return result;
   }
 
-  async findAll(): Promise<User[]> {
+  async findAll() {
     const results = await this.userRepository.find();
     return results;
   }
 
-  async findOne(id: number): Promise<User> {
+  async findOne(id: number) {
     const result = await this.userRepository.findOne(id);
+
+    // not found error
     if (!result) {
       throw new NotFoundException('The user is not found.');
     }
     return result;
   }
 
-  async update(id: number, user: CreateUserDto): Promise<User> {
-    const result = await this.userRepository.findOne(id);
-    if (!result) {
-      throw new NotFoundException('The user is not found.');
-    }
-    if (user.name && user.name != result.name) {
-      const existingName = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.name = :name', { name: user.name })
-        .getOne();
-      if (existingName) {
-        throw new ConflictException('The user already exists.');
-      }
-    }
-    await this.userRepository.update(id, user);
-    const updatedResult = await this.userRepository.findOne(id);
+  async update(id: number, user: UserParams) {
+    try {
+      const result = await this.userRepository.update(id, user);
 
-    return updatedResult;
+      // not found error
+      if (!result.affected)
+        throw new NotFoundException('The user is not found.');
+    } catch (err) {
+      // conflict error
+      if (err.code === 'ER_DUP_ENTRY')
+        throw new ConflictException('The user already exists.');
+    }
+
+    // updated result
+    const result = await this.userRepository.findOne(id);
+    return result;
   }
 
-  async remove(id: number): Promise<User> {
+  async remove(id: number) {
+    try {
+      const result = await this.userRepository.delete(id);
+
+      // not found error
+      if (!result.affected)
+        throw new NotFoundException('The user is not found.');
+    } catch (err) {}
+
+    // deleted result
     const result = await this.userRepository.findOne(id);
-    if (!result) {
-      throw new NotFoundException('The user is not found.');
-    }
-    await this.userRepository.delete(id);
     return result;
   }
 }
