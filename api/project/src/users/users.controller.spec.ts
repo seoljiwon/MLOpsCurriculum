e8173f9d2d8424ffa9a88fd1c195efd0245ad2d6
seol <suljiwon@naver.com>
2022-06-07 14:56:29 +0900

    feat: add test script for controller, service

--- api/project/src/users/users.controller.spec.ts
+++ api/project/src/users/users.controller.spec.ts
@@ -1,5 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+const mockUserService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +16,12 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +29,56 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    const params = {
+      name: 'seol',
+      age: 23,
+    };
+
+    it('should call create service with valid params', async () => {
+      await controller.create(params);
+      expect(mockUserService.create).toBeCalledWith(params);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call findAll service', async () => {
+      await controller.findAll();
+      expect(mockUserService.findAll).toBeCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    const id = 1;
+
+    it('should call findOne service with valid params', async () => {
+      await controller.findOne(id);
+      expect(mockUserService.findOne).toBeCalledWith(id);
+    });
+  });
+
+  describe('update', () => {
+    const id = 1;
+
+    const params = {
+      name: 'seol',
+      age: 23,
+    };
+
+    it('should call update service with valid params', async () => {
+      await controller.update(id, params);
+      expect(mockUserService.update).toBeCalledWith(id, params);
+    });
+  });
+
+  describe('remove', () => {
+    const id = 1;
+
+    it('should call remove service with valid params', async () => {
+      await controller.remove(id);
+      expect(mockUserService.remove).toBeCalledWith(id);
+    });
   });
 });
--- api/project/src/users/users.service.spec.ts
+++ api/project/src/users/users.service.spec.ts
@@ -1,18 +1,223 @@
+import { NotFoundException, ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { User } from './entity/user.entity';
+import { ErrorCode } from '../common/error.types';
+
+function erDupEntry() {
+  this.code = ErrorCode.ER_DUP_ENTRY;
+}
+
+let autoIncrementId = 1;
+const user1 = new User();
+user1.id = autoIncrementId++;
+user1.name = 'seol';
+user1.age = 24;
+
+let users: User[] = [];
+
+const mockUserRepo = {
+  find: jest.fn(() => {
+    return users;
+  }),
+  findOne: jest.fn((id: number) => {
+    if (id != undefined) {
+      const user = users.filter((u) => u.id === id)[0];
+      return user;
+    }
+    return null;
+  }),
+  save: jest.fn((user: Partial<User>) => {
+    users.map((u) => {
+      if (u.name === user.name) throw new erDupEntry();
+    });
+
+    const newUser = new User();
+    newUser.id = autoIncrementId++;
+    newUser.name = user.name;
+    if (user.age) newUser.age = user.age;
+    users.push(newUser);
+
+    return newUser;
+  }),
+  update: jest.fn((id: number, user: Partial<User>) => {
+    users.map((u) => {
+      if (u.id !== id && u.name === user.name) throw new erDupEntry();
+    });
+
+    const updatedUser = mockUserRepo.findOne(id);
+    if (!updatedUser) {
+      const result = {
+        affected: 0,
+      };
+      return result;
+    }
+
+    updatedUser.affected = 1;
+    if (user.name) updatedUser.name = user.name;
+    if (user.age) updatedUser.age = user.age;
+
+    return updatedUser;
+  }),
+  delete: jest.fn((id: number) => {
+    if (id !== undefined) {
+      const index = users.findIndex((c) => c.id === id);
+      if (index > -1) {
+        const deletedUser = users[index];
+        users.splice(index, 1);
+        return deletedUser;
+      }
+    }
+  }),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: mockUserRepo },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+
+    users = [user1];
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create new user', async () => {
+      const params = {
+        name: 'newName',
+        age: 20,
+      };
+
+      const newUser = await service.create(params);
+
+      expect(newUser).toBeDefined;
+      expect(newUser.name).toEqual(params.name);
+      expect(newUser.age).toEqual(params.age);
+    });
+
+    it('should throw conflict exception when duplicated', async () => {
+      const params = {
+        name: 'seol',
+        age: 22,
+      };
+
+      await expect(async () => await service.create(params)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should find all users', async () => {
+      await service.create({
+        name: 'myName',
+        age: 23,
+      });
+
+      const allUsers = await service.findAll();
+
+      expect(allUsers.length).toEqual(users.length);
+      expect(allUsers.map((u) => u)).toEqual(users.map((u) => u));
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find a user', async () => {
+      const id = 1;
+
+      const user = await service.findOne(id);
+
+      expect(user).toBeDefined;
+      expect(user.id).toEqual(id);
+      expect(user.name).toBeDefined();
+    });
+
+    it('should throw not found exception when no user', async () => {
+      const id = 999999;
+
+      await expect(async () => await service.findOne(id)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const id = 1;
+      const params = {
+        name: 'seoljiwon',
+        age: 20,
+      };
+
+      const updatedUser = await service.update(id, params);
+
+      expect(updatedUser.id).toEqual(id);
+      expect(updatedUser.name).toEqual(params.name);
+      expect(updatedUser.age).toEqual(params.age);
+    });
+
+    it('should throw not found exception when no user', async () => {
+      const id = 999999;
+      const params = {
+        name: 'seolji',
+        age: 20,
+      };
+
+      await expect(
+        async () => await service.update(id, params),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw conflict exception when duplicated', async () => {
+      await service.create({
+        name: 'myName',
+        age: 23,
+      });
+
+      const id = 1;
+      const params = {
+        name: 'myName',
+        age: 20,
+      };
+
+      await expect(
+        async () => await service.update(id, params),
+      ).rejects.toThrow(ConflictException);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user', async () => {
+      const id = 1;
+
+      const beforeDeleteLen = users.length;
+
+      const deletedUser = await service.remove(id);
+
+      const afterDeleteLen = users.length;
+
+      expect(deletedUser).toBeDefined;
+      expect(deletedUser.id).toEqual(id);
+      expect(beforeDeleteLen - 1).toEqual(afterDeleteLen);
+    });
+
+    it('should throw not found exception when no user', async () => {
+      const id = 999999;
+
+      await expect(async () => await service.remove(id)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
